0d46cadb3093af049f8961f06808b201400cdb9b
SergFediaev <sergfediaev@gmail.com>
2024-04-03 13:43:39 +0300

    Димыч, урок: 22 react кабзда как подробно useeffect settimeout setinterval.

--- src/lessons/react/useEffect/UseEffect.stories.tsx
+++ src/lessons/react/useEffect/UseEffect.stories.tsx
@@ -31,3 +31,45 @@ export const UseEffectExample = () => {
         <button onClick={() => setFake(fake + 1)}>Fake +</button>
     </>
 }
+
+export const SetTimeoutExample = () => {
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+    console.log('SetTimeoutExample')
+
+    /*    useEffect(() => {
+            setTimeout(() => {
+                console.log('setTimeout')
+                document.title = counter.toString()
+            }, 1_000)
+        }, [counter])*/
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log('setTimeout tick: ', counter)
+            // setCounter(counter + 1) // Из-за замыкания counter зациклится на значении 1 каждую секунду.
+            setCounter((state) => state + 1) // Реакт всегда передаёт актуальное состояние в переменную state.
+        }, 1_000)
+    }, [])
+
+    return <>
+        Hello, counter: {counter}, fake: {fake}
+        {/*<button onClick={() => setCounter(counter + 1)}>Counter +</button>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>Fake +</button>*/}
+    </>
+}
+
+export const TimeHomework = () => {
+    const [time, setTime] = useState('00:00:00')
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log('TimeHomework useEffect date')
+            const date = new Date()
+            const finalTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+            setTime(finalTime)
+        }, 1_000)
+    }, [])
+
+    return <>Time: {time}</>
+}
